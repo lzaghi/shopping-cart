1c6dd029b4900ef3cb4a5b2383ccb9b5008d2085
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-09-13 12:06:41 -0300

    requisito 7

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -1,9 +1,13 @@
+const { expect } = require('@jest/globals');
 const localStorageSimulator = require('../mocks/localStorageSimulator');
 const getSavedCartItems = require('../helpers/getSavedCartItems');
 
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('checks if localStorage.getItem is called and its parameter when getSavedCartItems()', () => {
+    getSavedCartItems();
+    expect(localStorage.getItem).toHaveBeenCalledTimes(1);
+    expect(localStorage.getItem).toHaveBeenCalledWith('cartItems')
+  });
 });
